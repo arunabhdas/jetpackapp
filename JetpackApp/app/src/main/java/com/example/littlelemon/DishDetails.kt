7e5c11fbeb37ebdfaf643d1646290f241d9631cd
Arunabh Das <arunabhdas@gmail.com>
2023-03-15 07:39:41 -0400

    Implemented detail screen

--- JetpackApp/app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ JetpackApp/app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,12 +1,19 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +22,35 @@ fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
         //TODO: Insert code here
+        TopAppBar()
+        Image(
+            painter = painterResource(id = dish.imageResource),
+            contentDescription = "Dish image",
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth,
+        )
+        Column() {
+            Text(
+                text = dish.name,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+            Text(
+                text = dish.description,
+                style = MaterialTheme.typography.body1,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+            Counter()
+            Button(onClick = { /*TODO*/ }) {
+               Text(
+                   text = stringResource(id = R.string.add_for) + " $${dish.price}"
+               )
+            }
+
+        }
+
     }
 }
 
@@ -55,7 +91,7 @@ fun Counter() {
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun DishDetailsPreview() {
     DishDetails(id = 1)
