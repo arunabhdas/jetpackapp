882dedfb1cae3c7d873d16166873a311cb0f8c24
Arunabh Das <arunabhdas@gmail.com>
2023-04-26 13:17:50 -0400

    Improved Unicorn App layout

--- UnicornApp/app/src/main/java/com/example/littlelemon/ItemDetails.kt
+++ UnicornApp/app/src/main/java/com/example/littlelemon/ItemDetails.kt
@@ -2,7 +2,6 @@ package com.example.littlelemon
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -10,7 +9,6 @@ import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
